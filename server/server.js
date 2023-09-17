cb58a515368f00f04fbedd6f9f7b5a1416ea5c94
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-18 01:24:26 +0530

    Create API Created

--- server/server.js
+++ server/server.js
@@ -6,7 +6,21 @@ import CrudModel from "./models/CrudModel.js";
 const app = express();
 const PORT = 8080;
 
+app.use(express.json())
+
+// Datbase Configuration
+
 connectDB()
+
+app.get('/',(req,resp)=>{
+resp.send("hiii")
+})
+
+app.post('/post',async(req,resp)=>{
+ const data =new CrudModel(req.body)
+ const save = await data.save()
+ resp.send(save)
+})
 
 app.listen(PORT,()=>{
     console.log(`Server Running on port ${PORT}`)
