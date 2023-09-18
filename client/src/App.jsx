e2bf677aa52b350ca2bab979658344e21b84a354
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-18 22:09:53 +0530

    Get the Created Data from the Database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
+  const [list, setList] = useState([]);
 
-  // Created Data in database
+  // Created Data in Database || Create
 
-  const PostData = async () => {
+  const CreateData = async () => {
     try {
       const data = await axios.post("http://localhost:8080/create-data", {
         name: name,
         age: age,
       });
-      console.log(data);
+      // console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  //Get Data From Database || Read
+
+  useEffect(() => {
+    const UpdateData = async () => {
+      try {
+        const result = await axios.get("http://localhost:8080/read-data");
+        console.log(result.data);
+        setList(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    UpdateData();
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <main>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter your Name...."
@@ -35,8 +53,16 @@ function App() {
           className="input"
           onChange={(e) => setAge(e.target.value)}
         />
-        <button onClick={PostData}>Submit</button>
+        <button onClick={CreateData}>Add</button>
       </form>
+      {list.map((value) => (
+        <div className="mapped_items">
+          <h1>{value.name}</h1>
+          <h1>{value.age}</h1>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      ))}
     </main>
   );
 }
